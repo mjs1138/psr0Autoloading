cd50f746071a99b3b7fa3b791dd69ce89e33e87c
mike-sullivan-egov <mike.sullivan@egov.com>
2018-03-20 13:23:46 -0400

    First working commit (Not Autoload)

---
+++ app/models/User.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mike
+ * Date: 3/20/18
+ * Time: 12:51 PM
+ */
+
+class User
+{
+    public function __construct()
+    {
+        die('Hello from the User Constructor');
+    }
+}
